ac027d87db28a6bdc2c096df0279b50438f39882
Mateo Montanez Baron <mmontanez@lumu.io>
2022-09-22 08:17:03 -0500

    Adding tooltip

--- app.js
+++ app.js
@@ -1,89 +1,114 @@
 const draw = async () => {
-    // Data
-    const dataset = await d3.json('./data.json');
+  // Data
+  const dataset = await d3.json("./data.json");
 
-    const xAccessor = (d) => d.currently.humidity;
-    const yAccessor = (d) => d.currently.apparentTemperature;
+  const xAccessor = (d) => d.currently.humidity;
+  const yAccessor = (d) => d.currently.apparentTemperature;
 
-    // Dimensions
-    let dimensions = {
-        width: 800,
-        height: 800,
-        margin: {
-            top: 50,
-            bottom: 50,
-            left: 50,
-            right: 50,
-        }
-    }
+  // Dimensions
+  let dimensions = {
+    width: 800,
+    height: 800,
+    margin: {
+      top: 50,
+      bottom: 50,
+      left: 50,
+      right: 50,
+    },
+  };
 
-    dimensions.ctrWith = dimensions.width - dimensions.margin.left - dimensions.margin.right;
-    dimensions.ctrHeight = dimensions.height - dimensions.margin.top - dimensions.margin.bottom;
+  dimensions.ctrWith =
+    dimensions.width - dimensions.margin.left - dimensions.margin.right;
+  dimensions.ctrHeight =
+    dimensions.height - dimensions.margin.top - dimensions.margin.bottom;
 
-    // Draw image
-    const svg = d3.select('#chart')
-        .append('svg')
-        .attr('width', dimensions.width)
-        .attr('height', dimensions.height);
+  // Draw image
+  const svg = d3
+    .select("#chart")
+    .append("svg")
+    .attr("width", dimensions.width)
+    .attr("height", dimensions.height);
 
-    const g = svg.append('g')
-        .attr('transform', `translate(${dimensions.margin.left}, ${dimensions.margin.top})`);
+  const g = svg
+    .append("g")
+    .attr(
+      "transform",
+      `translate(${dimensions.margin.left}, ${dimensions.margin.top})`
+    );
 
-    // Scales
-    const xScale = d3.scaleLinear()
-        .domain(d3.extent(dataset, xAccessor))
-        .rangeRound([0, dimensions.ctrWith])
-        .clamp(true);
+  const tooltip = d3.select("#tooltip");
 
-    const yScale = d3.scaleLinear()
-        .domain(d3.extent(dataset, yAccessor))
-        .rangeRound([dimensions.ctrHeight, 0])
-        // round decimal values only for the domain
-        .nice()
-        // prevent drawn a new shape outside of the container bounds 
-        .clamp(true);
+  // Scales
+  const xScale = d3
+    .scaleLinear()
+    .domain(d3.extent(dataset, xAccessor))
+    .rangeRound([0, dimensions.ctrWith])
+    .clamp(true);
 
-    // Draw Circle
-    g.selectAll('circle')
-        .data(dataset)
-        .join('circle')
-        .attr('cx', d => xScale(xAccessor(d)))
-        .attr('cy', d => yScale(yAccessor(d)))
-        .attr('r', 5)
-        .attr('fill', 'red')
-        .attr('data-temp', yAccessor);
+  const yScale = d3
+    .scaleLinear()
+    .domain(d3.extent(dataset, yAccessor))
+    .rangeRound([dimensions.ctrHeight, 0])
+    // round decimal values only for the domain
+    .nice()
+    // prevent drawn a new shape outside of the container bounds
+    .clamp(true);
 
-    // Axes
-    const xAxis = d3.axisBottom(xScale)
-        .ticks(5)
-        .tickFormat(label => label * 100 + '%');
-    // .tickValues([0.4, 0.5, 0.8])
+  // Draw Circle
+  g.selectAll("circle")
+    .data(dataset)
+    .join("circle")
+    .attr("cx", (d) => xScale(xAccessor(d)))
+    .attr("cy", (d) => yScale(yAccessor(d)))
+    .attr("r", 5)
+    .attr("fill", "red")
+    .attr("data-temp", yAccessor)
+    .on("mouseenter", function (event, datum) {
+      d3.select(this).attr("fill", "#120078").attr("r", 8);
 
-    const xAxisGroup = g.append('g')
-        .call(xAxis)
-        .style('transform', `translateY(${dimensions.ctrHeight}px)`)
-        .classed('axis', true);
+      tooltip
+        .style("display", "block")
+        .style("top", yScale(yAccessor(datum)) - 25 + "px")
+        .style("left", xScale(xAccessor(datum)) + "px");
+    })
+    .on("mouseleave", function (event) {
+      d3.select(this).attr("fill", "red").attr("r", 5);
 
-    xAxisGroup.append('text')
-        .attr('x', dimensions.ctrWith / 2)
-        .attr('y', dimensions.margin.bottom - 10)
-        .attr('fill', 'black')
-        .text('Humidity');
+      tooltip.style("display", "none");
+    });
 
-    const yAxis = d3.axisLeft(yScale);
+  // Axes
+  const xAxis = d3
+    .axisBottom(xScale)
+    .ticks(5)
+    .tickFormat((label) => label * 100 + "%");
+  // .tickValues([0.4, 0.5, 0.8])
 
-    const yAxisGroup = g.append('g')
-        .call(yAxis)
-        .classed('axis', true);
+  const xAxisGroup = g
+    .append("g")
+    .call(xAxis)
+    .style("transform", `translateY(${dimensions.ctrHeight}px)`)
+    .classed("axis", true);
 
-    yAxisGroup.append('text')
-        .attr('x', -dimensions.ctrHeight / 2)
-        .attr('y', -dimensions.margin.left + 15)
-        .attr('fill', 'black')
-        .html('Temperature &deg; F')
-        .style('transform', 'rotate(270deg)')
-        .style('text-anchor', 'middle')
+  xAxisGroup
+    .append("text")
+    .attr("x", dimensions.ctrWith / 2)
+    .attr("y", dimensions.margin.bottom - 10)
+    .attr("fill", "black")
+    .text("Humidity");
 
-}
+  const yAxis = d3.axisLeft(yScale);
+
+  const yAxisGroup = g.append("g").call(yAxis).classed("axis", true);
+
+  yAxisGroup
+    .append("text")
+    .attr("x", -dimensions.ctrHeight / 2)
+    .attr("y", -dimensions.margin.left + 15)
+    .attr("fill", "black")
+    .html("Temperature &deg; F")
+    .style("transform", "rotate(270deg)")
+    .style("text-anchor", "middle");
+};
 
 draw();
