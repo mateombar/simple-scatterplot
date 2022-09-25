e9f7f28f2ab192987cfe08bb9c116dc76eb30483
Mateo Montanez Baron <mmontanez@lumu.io>
2022-09-25 12:20:36 -0500

    Making the scatterplot a Voronoi diagram

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ const draw = async () => {
     .attr("width", dimensions.width)
     .attr("height", dimensions.height);
 
-  const g = svg
+  const ctr = svg
     .append("g")
     .attr(
       "transform",
@@ -55,36 +55,15 @@ const draw = async () => {
     .clamp(true);
 
   // Draw Circle
-  g.selectAll("circle")
+  ctr
+    .selectAll("circle")
     .data(dataset)
     .join("circle")
     .attr("cx", (d) => xScale(xAccessor(d)))
     .attr("cy", (d) => yScale(yAccessor(d)))
     .attr("r", 5)
     .attr("fill", "red")
-    .attr("data-temp", yAccessor)
-    .on("mouseenter", function (event, datum) {
-      d3.select(this).attr("fill", "#120078").attr("r", 8);
-
-      tooltip
-        .style("display", "block")
-        .style("top", yScale(yAccessor(datum)) - 25 + "px")
-        .style("left", xScale(xAccessor(datum)) + "px");
-
-      const formatter = d3.format(".2f");
-      const dateFormatter = d3.timeFormat("%B %-d, %Y");
-
-      tooltip.select(".metric-humidity span").text(formatter(xAccessor(datum)));
-
-      tooltip.select(".metric-temp span").text(formatter(yAccessor(datum)));
-
-      tooltip.select(".metric-date").text(dateFormatter(datum.currently.time * 1000));
-    })
-    .on("mouseleave", function (event) {
-      d3.select(this).attr("fill", "red").attr("r", 5);
-
-      tooltip.style("display", "none");
-    });
+    .attr("data-temp", yAccessor);
 
   // Axes
   const xAxis = d3
@@ -93,7 +72,7 @@ const draw = async () => {
     .tickFormat((label) => label * 100 + "%");
   // .tickValues([0.4, 0.5, 0.8])
 
-  const xAxisGroup = g
+  const xAxisGroup = ctr
     .append("g")
     .call(xAxis)
     .style("transform", `translateY(${dimensions.ctrHeight}px)`)
@@ -108,7 +87,7 @@ const draw = async () => {
 
   const yAxis = d3.axisLeft(yScale);
 
-  const yAxisGroup = g.append("g").call(yAxis).classed("axis", true);
+  const yAxisGroup = ctr.append("g").call(yAxis).classed("axis", true);
 
   yAxisGroup
     .append("text")
@@ -118,6 +97,54 @@ const draw = async () => {
     .html("Temperature &deg; F")
     .style("transform", "rotate(270deg)")
     .style("text-anchor", "middle");
+
+  const delaunay = d3.Delaunay.from(
+    dataset,
+    (d) => xScale(xAccessor(d)),
+    (d) => yScale(yAccessor(d))
+  );
+  const voronoi = delaunay.voronoi();
+  voronoi.xmax = dimensions.ctrWith;
+  voronoi.ymax = dimensions.ctrHeight;
+
+  ctr
+    .append("g")
+    .selectAll("path")
+    .data(dataset)
+    .join("path")
+    .attr("fill", "transparent")
+    .attr("d", (d, i) => voronoi.renderCell(i))
+    .on("mouseenter", function (event, datum) {
+      ctr
+        .append("circle")
+        .classed("dot-hovered", true)
+        .attr("fill", "#120078")
+        .attr("r", 8)
+        .attr("cx", (d) => xScale(xAccessor(datum)))
+        .attr("cy", (d) => yScale(yAccessor(datum)))
+        .style('pointer-events', 'none')
+
+      tooltip
+        .style("display", "block")
+        .style("top", yScale(yAccessor(datum)) - 25 + "px")
+        .style("left", xScale(xAccessor(datum)) + "px");
+
+      const formatter = d3.format(".2f");
+      const dateFormatter = d3.timeFormat("%B %-d, %Y");
+
+      tooltip.select(".metric-humidity span").text(formatter(xAccessor(datum)));
+
+      tooltip.select(".metric-temp span").text(formatter(yAccessor(datum)));
+
+      tooltip
+        .select(".metric-date")
+        .text(dateFormatter(datum.currently.time * 1000));
+    })
+    .on("mouseleave", function (event) {
+      ctr.select(".dot-hovered").remove();
+
+      tooltip.style("display", "none");
+    });
 };
 
 draw();
